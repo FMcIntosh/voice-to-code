2d2b05ff512971556c4f34f9ac7493ade981ed73
Fraser McIntosh <frasermcintosh1@gmail.com>
2018-03-16 00:55:39 +1300

    Refactor speech recognition module

--- pages/index.js
+++ pages/index.js
@@ -5,67 +5,46 @@ import TextField from 'material-ui/TextField';
 import Button from 'material-ui/Button';
 import CodeSection from 'components/CodeSection';
 import tts from 'util/textToSpeech';
+import speechRecognition from 'util/speechRecognition';
 
 export default class extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { color: '' };
+    this.state = { spokenText: '' };
   }
 
   componentDidMount() {
-    var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
-
-    this.recognition = new SpeechRecognition();
-    //this.recognition.continuous = false;
-    this.recognition.lang = 'en-US';
-    this.recognition.interimResults = false;
-    this.recognition.maxAlternatives = 1;
-
-    this.recognition.onresult = (event, obj = this) => {
-      var last = event.results.length - 1;
-      obj.setState({ color: event.results[last][0].transcript });
-
-      console.log('Confidence: ' + event.results[0][0].confidence);
-
-      tts(event.results[last][0].transcript);
-    };
-
-    this.recognition.onspeechend = () => {
-      this.recognition.stop();
-    };
-
-    this.recognition.onnomatch = () => {
-      diagnostic.textContent = "I didn't recognise that color.";
-    };
-
-    this.recognition.onerror = event => {
-      diagnostic.textContent = 'Error occurred in this.recognition: ' + event.error;
-    };
+    this.setState({ SpeechRecognition: new speechRecognition(this.onSpeechResult) });
   }
 
-  eval = () => {
-    return eval(`${this.state.color}`);
-  }
+  onSpeechResult = event => {
+    const last = event.results.length - 1;
+    const spokenText = event.results[last][0].transcript;
+    this.setState({ spokenText });
+    tts(spokenText);
+  };
+
+  evalCode = () => {
+    return eval(`${this.state.spokenText}`);
+  };
 
   render() {
     return (
       <Grid container direction="column" align="center">
         <h2>Click start to have your speech recorded to text</h2>
-        <StyledField id="oppenness" multiline value={this.state.color} margin="dense" />
+        <StyledField id="oppenness" multiline value={this.state.spokenText} margin="dense" />
         <Button
           primary
           onClick={() => {
-            this.recognition.start();
-            this.setState({ color: '' });
+            this.state.SpeechRecognition.start();
+            this.setState({ spokenText: '' });
           }}
         >
           Start
         </Button>
-        <CodeSection>
-          {this.state.color}
-        </CodeSection>
-        output = {this.eval()}
+        <CodeSection>{this.state.spokenText}</CodeSection>
+        output = {this.evalCode()}
       </Grid>
     );
   }
